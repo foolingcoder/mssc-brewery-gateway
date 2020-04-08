1b95f49a35bb3012e720c7d68a77021e30dce283
angump <angump@host.docker.internal>
2020-04-08 11:21:57 +0400

    Added localhost gateway to beerorderservice

--- src/main/java/guru/sfg/brewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/guru/sfg/brewerygateway/config/LocalHostRouteConfig.java
@@ -11,9 +11,13 @@ public class LocalHostRouteConfig {
 	@Bean
 	public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(r-> r.path("/api/v1/beer/*", "/api/v1/beer*", "/api/v1/beerUpc/*")
-				.uri("http://localhost:8081")
-				.id("beer-service"))
+				.route(r-> r.path(("/api/v1/beer/*"), ("/api/v1/beer*"), ("/api/v1/beerUpc/*"))
+							.uri("http://localhost:8081")
+							.id("beer-service"))
+				
+				.route(r -> r.path(("/api/v1/customers/**"), ("api/v1/customers*"))
+						   .uri("http://localhost:8082")
+						   .id("order-service"))
 				.build();
 
 	}
