c9a4f973e3208866633b1aef4eedd908207138a1
angump <angump@host.docker.internal>
2020-04-10 19:41:36 +0400

    Changed the port numbers  of the services

--- src/main/java/guru/sfg/brewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/guru/sfg/brewerygateway/config/LocalHostRouteConfig.java
@@ -12,15 +12,15 @@ public class LocalHostRouteConfig {
 	public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r-> r.path(("/api/v1/beer/*"), ("/api/v1/beer*"), ("/api/v1/beerUpc/*"))
-							.uri("http://localhost:8081")
+							.uri("http://localhost:8080")
 							.id("beer-service"))
 				
 				.route(r -> r.path(("/api/v1/customers/**"), ("api/v1/customers*"))
-						   .uri("http://localhost:8082")
+						   .uri("http://localhost:8081")
 						   .id("order-service"))
 				
 				.route(r -> r.path(("/api/v1/beer/*/inventory"))
-						   .uri("http://localhost:8083")
+						   .uri("http://localhost:8082")
 						   .id("inventory-service"))
 				.build();
 
